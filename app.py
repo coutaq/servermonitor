a0f55c913f600d1f3bd45c6b18fbbf6a8ca4ac95
Coutaq <7tGu9ZQn>
2021-11-08 00:57:18 +0300

    asd

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ def index():
     return {
         "cpu": get_cpu(),
         "mem": get_mem(),
+        "disks": get_disk(),
+        "network": get_network(),
+        "sensors": get_sensors()
     }
 
 def getStr(value, multiplier, accuracy, suffix):
@@ -25,10 +28,52 @@ def get_cpu():
     }
 def get_mem():
     mem = psutil.virtual_memory()
-    print(mem)
     return {
         "total":getStr(mem[0], 1/(STEP**3), 1, " GB"),
         "available":getStr(mem[0]-mem[3], 1/(STEP**3), 1, " GB"),
         "percent": getStr(mem[2], 1, 1, "%"),
         "used": getStr(mem[3], 1/(STEP**3), 1, " GB"),
         }
+    
+def get_disk():
+    drives = list()
+    disks = list(filter(lambda d: d[2], psutil.disk_partitions()))
+    for disk in disks:
+        storage = psutil.disk_usage(disk[1])
+        drives.append({
+            "device": disk[0],
+            "mountpoint": disk[1],
+            "fstype": disk[2],
+            "storage":{
+                 "total": getStr(storage[0], 1/(STEP**3), 1, " GB"),
+                 "available": getStr(storage[2], 1/(STEP**3), 1, " GB"),
+                 "percent": getStr(storage[3], 1, 1, "%"),
+                 "used": getStr(storage[1], 1/(STEP**3), 1, " GB"),
+            }
+        })
+    return drives
+
+
+def get_network():
+    # conns = list(filter(lambda c: c[1],psutil.net_if_stats()))
+    conns = list(filter(lambda c: c[1],[(k, *v) for k, v in psutil.net_if_stats().items()]))
+    connections = list()
+    for conn in conns:
+        connections.append({
+            "name": conn[0],
+            "speed": getStr(conn[3], 1,1," MB"),
+        })
+    return connections
+
+
+def get_sensors():
+    sensors = list()
+    try:
+        sensors.append(psutil.sensors_temperatures())
+    except Exception:
+        pass
+    try:
+        sensors.append(psutil.sensors_fans())
+    except Exception:
+        pass
+    return sensors
