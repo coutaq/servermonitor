59f9833d5096abd135b1cf6c3559c55deeaa9741
Coutaq <7tGu9ZQn>
2021-11-08 00:18:26 +0300

    asdsad

--- app.py
+++ app.py
@@ -7,11 +7,21 @@ app = Flask(__name__)
 def index():
     load = get_load()
     return {
-        "cpu": load[0],
-        "mem": load[1],
+        "cpu": get_cpu,
+        "mem": get_mem(),
     }
 
-def get_load():
+def get_cpu():
+    cores = psutil.cpu_count(logical=False)
+    threads = psutil.cpu_count(logical=True)
+    total_load = psutil.cpu_percent(1)
+    cores_load = psutil.cpu_percent(1, percpu=True)
+    return {
+        cores,
+        threads,
+        total_load,
+        cores_load
+    }
+def get_mem():
     mem = psutil.virtual_memory()
-    cpu = psutil.cpu_percent(1)
-    return(cpu,mem)
+    return mem
