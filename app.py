7b9ee3d640ae7266f7eff579e9c7970c13e96d16
Coutaq <coutaq@gmail.com>
2021-11-09 18:14:39 +0300

    dasda

--- app.py
+++ app.py
@@ -2,45 +2,58 @@ from flask import Flask, jsonify, request, abort
 import psutil
 import hashlib
 from flask_sock import Sock
+import speedtest
 
 app = Flask(__name__)
 sock = Sock(app)
 STEP = 1024
 api_keys = list()
 
+
 def get_keys():
     f = open("APIKEYS", "r")
     for x in f:
-      if(x):
-          api_keys.append( hashlib.sha256(x.strip().encode()).hexdigest())
+        if(x):
+            api_keys.append(hashlib.sha256(x.strip().encode()).hexdigest())
     f.close()
-    
+
+
 get_keys()
+
 
 @app.route("/", methods=["GET"])
 def index():
     check_key(request.args.get("key"))
     return get_data()
-    
+
+
 @sock.route('/data')
 def echo(ws):
     check_key(request.args.get("key"))
     while True:
         ws.send(get_data())
+
+
 def check_key(key):
     if hashlib.sha256(str(key).strip().encode()).hexdigest() not in api_keys:
         abort(401)
+
+
 def get_str(value, multiplier, accuracy, suffix):
     return str(round(value*multiplier, accuracy))+suffix
 
+
 def get_data():
-      return {
+    return {
         "cpu": get_cpu(),
         "mem": get_mem(),
         "disks": get_disk(),
         "network": get_network(),
-        "temps": get_temps()
-      }
+        "temps": get_temps(),
+        "speed": get_speed()
+    }
+
+
 def get_cpu():
     cores = psutil.cpu_count(logical=False)
     threads = psutil.cpu_count(logical=True)
@@ -48,30 +61,33 @@ def get_cpu():
     cores_load = psutil.cpu_percent(0, percpu=True)
     return {
         "cores": cores,
-        "threads":threads,
-        "total_load":total_load,
-        "threads_load":cores_load
+        "threads": threads,
+        "total_load": total_load,
+        "threads_load": cores_load
     }
+
+
 def get_mem():
     mem = psutil.virtual_memory()
     return {
-        "total":get_str(mem[0], 1/(STEP**3), 1, " GB"),
-        "available":get_str(mem[0]-mem[3], 1/(STEP**3), 1, " GB"),
+        "total": get_str(mem[0], 1/(STEP**3), 1, " GB"),
+        "available": get_str(mem[0]-mem[3], 1/(STEP**3), 1, " GB"),
         "percent": get_str(mem[2], 1, 1, "%"),
         "used": get_str(mem[3], 1/(STEP**3), 1, " GB"),
-        }
-    
+    }
+
+
 def get_disk():
     disks = list()
     for disk in psutil.disk_partitions():
         if(disk[2]):
             storage = psutil.disk_usage(disk[1])
-            if(storage[0]>0.2*(STEP**3)):
+            if(storage[0] > 0.2*(STEP**3)):
                 disks.append({
                     "device": disk[0],
                     "mountpoint": disk[1],
                     "fstype": disk[2],
-                    "storage":{
+                    "storage": {
                         "total": get_str(storage[0], 1/(STEP**3), 1, " GB"),
                         "available": get_str(storage[2], 1/(STEP**3), 1, " GB"),
                         "percent": get_str(storage[3], 1, 1, "%"),
@@ -81,13 +97,21 @@ def get_disk():
     return disks
 
 
+def get_speed():
+    test = speedtest.Speedtest()
+    return{
+        "up": test.upload(),
+        "down": test.download()
+    }
+
+
 def get_network():
     conns = list()
     for conn in [(k, *v) for k, v in psutil.net_if_stats().items()]:
-        if(conn[1] and conn[3]>0):
+        if(conn[1] and conn[3] > 0):
             conns.append({
                 "name": conn[0],
-                "speed": get_str(conn[3], 1,1," MB"),
+                "speed": get_str(conn[3], 1, 1, " MB"),
             })
     return conns
 
@@ -96,7 +120,7 @@ def get_temps():
     temps = list()
     if not hasattr(psutil, "sensors_temperatures"):
         return temps
-    
+
     # try:
     for name, entries in psutil.sensors_temperatures().items():
         # t = temp._asdict(
@@ -106,7 +130,7 @@ def get_temps():
             "critical": t[3],
             "current": t[1],
             "high": t[2]
-            })
+        })
     # except Exception:
     #     pass
     # try:
