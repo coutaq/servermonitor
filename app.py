8a54eacbaa3880b904e55ea674a2c79eae800343
Coutaq <coutaq@gmail.com>
2021-11-09 10:29:01 +0300

    asd

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def get_network():
 def get_temps():
     temps = list()
     # try:
-    for t in psutil.sensors_temperatures().items:
+    for t in psutil.sensors_temperatures()._asdict():
         print(t)
         # temps.append({
         #     "name": t[0],
